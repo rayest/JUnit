1d7ed6c3238dfb1e43fd73a956043d730caa1c09
Rayest <yi@rayest.cn>
2016-08-12 13:31:55 +0800

    改进 testProcessRequest

---
+++ src/main/java/cn/ipay/controller/ErrorResponse.java
@@ -0,0 +1,37 @@
+package cn.ipay.controller;
+
+/**
+ * Created by Rayest on 2016/8/12 0012.
+ */
+public class ErrorResponse implements Response {
+
+    private static final String NAME = "Error";
+    private Request originRequest;
+    private Exception originException;
+
+    public ErrorResponse(Request request, Exception exception) {
+
+    }
+
+    public Request getOriginRequest() {
+        return originRequest;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public void setOriginRequest(Request originRequest) {
+        this.originRequest = originRequest;
+    }
+
+    public Exception getOriginException() {
+        return originException;
+    }
+
+    public void setOriginException(Exception originException) {
+        this.originException = originException;
+    }
+
+
+}
