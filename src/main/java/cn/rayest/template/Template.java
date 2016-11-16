03f9cc161db36ac3ea0d4653caed2aa67bcf37b1
Rayest <634178463@qq.com>
2016-11-16 23:54:02 +0800

    修改测试类和实现类

--- src/main/java/cn/rayest/template/Template.java
+++ src/main/java/cn/rayest/template/Template.java
@@ -4,16 +4,16 @@ package cn.rayest.template;
  * Created by Rayest on 2016/11/16 0016.
  */
 public class Template {
+    private String variableValue;
 
     public Template(String templateText) {
-
     }
 
     public void set(String variable, String value){
-
+        this.variableValue = value;
     }
 
     public String evaluate(){
-        return "Hello, Reader";
+        return "Hello, " + variableValue;
     }
 }
